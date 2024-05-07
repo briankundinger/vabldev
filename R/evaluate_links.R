ce89ed70bb0b91f1002bee25168f91d63b1725e3
briankundinger <briankundinger@gmail.com>
2024-05-07 15:52:21 -0400

    intersect

--- R/evaluate_links.R
+++ R/evaluate_links.R
@@ -23,10 +23,20 @@ evaluate_links <- function(Z_hat, Z_true, n1, format = "Z"){
 
     n_links <- dim(Z_hat)[1]
     n_matches <- dim(Z_true)[1]
-    n_correct_links <- apply(Z_hat, 1, function(x){
-      any(Z_true[, 1] == x[1] & Z_true[, 2] == x[2])
-    }) %>%
-      sum()
+    Z_hat_pair <- Z_hat %>%
+      data.frame() %>%
+      tidyr::unite("pair")
+
+    Z_true_pair <- Z_true %>%
+      data.frame() %>%
+      tidyr::unite("pair")
+
+    n_correct_links <- intersect(Z_hat_pair, Z_true_pair) %>%
+      length()
+    # n_correct_links <- apply(Z_hat, 1, function(x){
+    #   any(Z_true[, 1] == x[1] & Z_true[, 2] == x[2])
+    # }) %>%
+    #   sum()
 
     recall <- n_correct_links/n_matches
     precision <- n_correct_links/n_links
